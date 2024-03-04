d8e41c196c98f2dd49265631ec22177d6a29f864
User-Han <142723546+UserHan9@users.noreply.github.com>
2024-03-04 09:28:43 +0700

    Push

--- app/Http/Controllers/DataPeminjamanController.php
+++ app/Http/Controllers/DataPeminjamanController.php
@@ -7,6 +7,35 @@ use App\Models\DataPeminjaman;
 
 class DataPeminjamanController extends Controller
 {
+
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nama' => 'required|string',
+            'kelas' => 'required|string',
+            'data_barang_id' => 'required|exists:data_barang,id',
+        ]);
+    
+        $peminjaman = DataPeminjaman::create($data);
+    
+        // Mengambil informasi barang berdasarkan ID yang dipinjam
+        $barang = $peminjaman->barang;
+    
+        // Membuat array respons dengan informasi yang diminta
+        $response = [
+            'nama' => $peminjaman->nama,
+            'kelas' => $peminjaman->kelas,
+            'nama_barang' => $barang->nama_barang,
+            'merek' => $barang->merek,
+            'ruang' => $barang->ruang,
+            'status' => 'Dipinjam'
+        ];
+    
+        return response()->json(['message' => 'Data peminjaman berhasil ditambahkan', 'data' => $response], 201);
+    }
+
+
     public function update(Request $request, $id)
     {
         $peminjaman = DataPeminjaman::findOrFail($id);
